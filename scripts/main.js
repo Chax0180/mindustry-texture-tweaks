9766c87ff211c7b54b247dec0d7a20f4b52560a0
Chax0180 <chaxbent1324@gmail.com>
2026-04-28 11:27:19 -0400

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+
+var atlas = require("atlas");
+
+// init content
+require("content");
+
+Events.on(EventType.ClientLoadEvent, 
+cons(e => {
+	Log.info("Client load")
+
+	utils.eachType(ContentType.item, item=>{
+		atlas.setupIcon("item",item.name,item.fullIcon);
+	});
